41e53183acd0078e4af3fdbd8cd47fdf7b41b5d4
Chris Byrohl <cbyrohl@workstation-cbyrohl.MPA-Garching.MPG.DE>
2022-01-14 20:50:04 +0100

    introduce particle type aliases

--- darepo/helpers_misc.py
+++ darepo/helpers_misc.py
@@ -33,3 +33,25 @@ def make_serializable(v):
     if isinstance(v, bytes):
         v = v.decode("utf-8")
     return v
+
+def partTypeNum(partType):
+    """ Mapping between common names and numeric particle types. """
+    if str(partType).isdigit():
+        return int(partType)
+
+    if str(partType).lower() in ['gas','cells']:
+        return 0
+    if str(partType).lower() in ['dm','darkmatter']:
+        return 1
+    if str(partType).lower() in ['dmlowres']:
+        return 2 # only zoom simulations, not present in full periodic boxes
+    if str(partType).lower() in ['tracer','tracers','tracermc','trmc']:
+        return 3
+    if str(partType).lower() in ['star','stars','stellar']:
+        return 4 # only those with GFM_StellarFormationTime>0
+    if str(partType).lower() in ['wind']:
+        return 4 # only those with GFM_StellarFormationTime<0
+    if str(partType).lower() in ['bh','bhs','blackhole','blackholes']:
+        return 5
+    if str(partType).lower() in ["all"]:
+        return -1
