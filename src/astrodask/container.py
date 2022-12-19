97972559ba7a78a9019ef59df875994dc8e04da8
Chris Byrohl <chris.byrohl@mailbox.org>
2022-12-19 16:24:30 +0100

    add container skeleton

---
+++ src/astrodask/container.py
@@ -0,0 +1,20 @@
+import os
+
+class Container(object):
+    """A container for collection of interface instances"""
+    def __init__(self, path):
+        self.path = path
+
+class DirectoryCatalog(object):
+    """A catalog consisting of interface instances contained in a directory."""
+    def __init__(self, path):
+        self.path = path
+
+class ArepoSimulation(Container):
+    """A container for an arepo simulation."""
+    def __init__(self, path):
+        super().__init__(path)
+        self.name = os.path.basename(self.path)
+        self.outpath = os.path.join(self.path, 'output')
+        self.snapcat = DirectoryCatalog(self.outpath)
+        self.groupcat = DirectoryCatalog(self.outpath)
