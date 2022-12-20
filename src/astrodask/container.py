80d8647b17fa5c06cdb79dc87737d753382fe07b
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2022-12-20 18:06:37 +0100

    format

--- src/astrodask/container.py
+++ src/astrodask/container.py
@@ -1,20 +1,26 @@
 import os
 
-class Container(object):
+
+class BaseContainer(object):
     """A container for collection of interface instances"""
+
     def __init__(self, path):
         self.path = path
+
 
 class DirectoryCatalog(object):
     """A catalog consisting of interface instances contained in a directory."""
+
     def __init__(self, path):
         self.path = path
 
-class ArepoSimulation(Container):
+
+class ArepoSimulation(BaseContainer):
     """A container for an arepo simulation."""
+
     def __init__(self, path):
         super().__init__(path)
         self.name = os.path.basename(self.path)
-        self.outpath = os.path.join(self.path, 'output')
+        self.outpath = os.path.join(self.path, "output")
         self.snapcat = DirectoryCatalog(self.outpath)
         self.groupcat = DirectoryCatalog(self.outpath)
