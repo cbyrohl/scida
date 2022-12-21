f59696f603489fddafcbeea2a47f96fa20c7bc43
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2022-12-21 17:37:18 +0100

    format

--- src/astrodask/interface.py
+++ src/astrodask/interface.py
@@ -18,11 +18,13 @@ from collections.abc import MutableMapping
 
 
 class Dataset(object):
-    def __init__(self, path, chunksize="auto", virtualcache=True, overwritecache = False):
+    def __init__(self, path, chunksize="auto", virtualcache=True, overwritecache=False):
         super().__init__()
         self.path = path
         self.file = None
-        self.tempfile = None  # need this reference to keep garbage collection away for the tempfile
+        self.tempfile = (
+            None  # need this reference to keep garbage collection away for the tempfile
+        )
         self.location = None
         self.chunksize = chunksize
         self.virtualcache = virtualcache
@@ -33,10 +35,10 @@ class Dataset(object):
         self.data = FieldContainerCollection()
 
         if not os.path.exists(self.path):
-            raise Exception("Specified path '%s' does not exist."%self.path)
+            raise Exception("Specified path '%s' does not exist." % self.path)
 
         if os.path.isdir(path):
-            if os.path.isfile(os.path.join(path,".zgroup")):
+            if os.path.isfile(os.path.join(path, ".zgroup")):
                 # object is a zarr object
                 self.load_zarr()
             else:
@@ -47,7 +49,7 @@ class Dataset(object):
             self.load_hdf5()
 
     # TODO: This does not work as intended. We seem to close the file for other instances as well (?) => problem
-    #def __del__(self):
+    # def __del__(self):
     #    """
     #    On deletion of object, make sure we close file.
     #    """
@@ -59,35 +61,42 @@ class Dataset(object):
     def __hash__(self):
         """Hash for Dataset instance to be derived from the file location."""
         # determinstic hash; note that hash() on a string is no longer deterministic in python3.
-        hash_value = int(hashlib.sha256(self.location.encode('utf-8')).hexdigest(), 16) % 10**10
+        hash_value = (
+            int(hashlib.sha256(self.location.encode("utf-8")).hexdigest(), 16)
+            % 10**10
+        )
         return hash_value
 
-        
     def __dask_tokenize__(self):
         return self.__hash__()
-
 
     def load_hdf5(self):
         self.location = self.path
         tree = {}
-        walk_hdf5file(self.location,tree=tree)
+        walk_hdf5file(self.location, tree=tree)
         try:
             import h5pickle
-            self.file = h5pickle.File(self.location,"r")
+
+            self.file = h5pickle.File(self.location, "r")
         except ImportError:
-            self.file = h5py.File(self.location,"r")
+            self.file = h5py.File(self.location, "r")
         self.load_from_tree(tree)
 
     def load_zarr(self):
         self.location = self.path
         tree = {}
-        walk_zarrfile(self.location,tree=tree)
+        walk_zarrfile(self.location, tree=tree)
         self.file = zarr.open(self.location)
         self.load_from_tree(tree)
 
-    def load_chunkedhdf5(self,overwrite=False):
+    def load_chunkedhdf5(self, overwrite=False, fileprefix=""):
         files = [os.path.join(self.path, f) for f in os.listdir(self.path)]
-        files = np.array([f for f in files if os.path.isfile(f)]) # ignore subdirectories
+        files = [f for f in files if os.path.isfile(f)]  # ignore subdirectories
+        files = np.array([f for f in files if f.split("/")[-1].startswith(fileprefix)])
+        prfxs = [f.split(".")[0] for f in files]
+        assert (
+            len(set(prfxs)) == 1
+        ), "more than one file prefix in directory, specify fileprefix"
         nmbrs = [int(f.split(".")[-2]) for f in files]
         sortidx = np.argsort(nmbrs)
         files = files[sortidx]
@@ -96,7 +105,7 @@ class Dataset(object):
         if "cachedir" in _config:
             # we create a virtual file in the cache directory
             dataset_cachedir = os.path.join(_config["cachedir"], hash_path(self.path))
-            fp = os.path.join(dataset_cachedir,"data.hdf5")
+            fp = os.path.join(dataset_cachedir, "data.hdf5")
             if not os.path.exists(dataset_cachedir):
                 os.mkdir(dataset_cachedir)
             if not os.path.isfile(fp) or overwrite:
@@ -107,21 +116,22 @@ class Dataset(object):
             self.tempfile = tempfile.NamedTemporaryFile("wb", suffix=".hdf5")
             self.location = self.tempfile.name
             create_mergedhdf5file(self.location, files, virtual=self.virtualcache)
-            logging.warning("No caching directory specified. Initial file read will remain slow.")
-
+            logging.warning(
+                "No caching directory specified. Initial file read will remain slow."
+            )
 
         try:
             import h5pickle
-            self.file = h5pickle.File(self.location,"r")
+
+            self.file = h5pickle.File(self.location, "r")
         except ImportError:
-            self.file = h5py.File(self.location,"r")
+            self.file = h5py.File(self.location, "r")
 
         tree = {}
         walk_hdf5file(self.location, tree)
         self.load_from_tree(tree)
 
-
-    def load_from_tree(self,tree,groups_load=None):
+    def load_from_tree(self, tree, groups_load=None):
         """groups_load: list of groups to load; all groups with datasets are loaded if groups_load==None"""
         inline_array = False  # inline arrays in dask; intended to improve dask scheduling (?). However, doesnt work with h5py (need h5pickle wrapper or zarr).
         if type(self.file) == h5py._hl.files.File:
@@ -136,7 +146,7 @@ class Dataset(object):
             if groups_load is not None:
                 toload = any([group.startswith(gname) for gname in groups_load])
             else:
-                toload = any([group=="/"+g for g in groups_with_datasets])
+                toload = any([group == "/" + g for g in groups_with_datasets])
             if toload:
                 self.data[group.split("/")[1]] = {}
         ## datasets
@@ -144,17 +154,36 @@ class Dataset(object):
             if groups_load is not None:
                 toload = any([dataset[0].startswith(gname) for gname in groups_load])
             else:
-                toload = any([dataset[0].startswith("/"+g+"/") for g in groups_with_datasets])
+                toload = any(
+                    [dataset[0].startswith("/" + g + "/") for g in groups_with_datasets]
+                )
             if toload:
-                group = dataset[0].split("/")[1]  # TODO: Still dont support more nested groups
-                name = "Dataset"+str(self.__dask_tokenize__())+dataset[0].replace("/","_")
-                if "__dask_tokenize__" in self.file: # check if the file contains the dask name.
-                    name = self.file["__dask_tokenize__"].attrs.get(dataset[0].strip("/"), name)
-                ds = da.from_array(self.file[dataset[0]], chunks=self.chunksize, name=name, inline_array=inline_array)
+                group = dataset[0].split("/")[
+                    1
+                ]  # TODO: Still dont support more nested groups
+                name = (
+                    "Dataset"
+                    + str(self.__dask_tokenize__())
+                    + dataset[0].replace("/", "_")
+                )
+                if (
+                    "__dask_tokenize__" in self.file
+                ):  # check if the file contains the dask name.
+                    name = self.file["__dask_tokenize__"].attrs.get(
+                        dataset[0].strip("/"), name
+                    )
+                ds = da.from_array(
+                    self.file[dataset[0]],
+                    chunks=self.chunksize,
+                    name=name,
+                    inline_array=inline_array,
+                )
                 self.data[group][dataset[0].split("/")[-1]] = ds
 
         ## Make each datadict entry a FieldContainer
-        data = FieldContainerCollection(self.data.keys(), derivedfields_kwargs=dict(snap=self))
+        data = FieldContainerCollection(
+            self.data.keys(), derivedfields_kwargs=dict(snap=self)
+        )
         for k in self.data:
             data.new_container(k, **self.data[k])
         self.data = data
@@ -163,7 +192,7 @@ class Dataset(object):
         for p in self.data:
             for k, v in self.data[p].items():
                 nparts = v.shape[0]
-                if len(v.chunks)==1:
+                if len(v.chunks) == 1:
                     # make same shape as other 1D arrays.
                     chunks = v.chunks
                     break
@@ -176,8 +205,6 @@ class Dataset(object):
         ## attrs
         self.metadata = tree["attrs"]
 
-
-
     def return_data(self):
         return self.data
 
@@ -189,7 +216,7 @@ class Dataset(object):
         # cast_uints: if true, we cast uints to ints; needed for some compressions (particularly zfp)
         if zarr_kwargs is None:
             zarr_kwargs = {}
-        store = zarr.DirectoryStore(fname,**zarr_kwargs)
+        store = zarr.DirectoryStore(fname, **zarr_kwargs)
         root = zarr.group(store, overwrite=overwrite)
 
         # Metadata
@@ -208,11 +235,13 @@ class Dataset(object):
             for k in self.data[p]:
                 arr = self.data[p][k]
                 if cast_uints:
-                    if arr.dtype==np.uint64:
+                    if arr.dtype == np.uint64:
                         arr = arr.astype(np.int64)
-                    elif arr.dtype==np.uint32:
+                    elif arr.dtype == np.uint32:
                         arr = arr.astype(np.int32)
-                task = da.to_zarr(arr, os.path.join(fname, p, k), overwrite=True, compute=False)
+                task = da.to_zarr(
+                    arr, os.path.join(fname, p, k), overwrite=True, compute=False
+                )
                 tasks.append(task)
         dask.compute(tasks)
 
@@ -221,30 +250,30 @@ class BaseSnapshot(Dataset):
     def __init__(self, path, chunksize="auto", virtualcache=True):
         super().__init__(path, chunksize=chunksize, virtualcache=virtualcache)
 
-        defaultattributes = ["config","header","parameters"]
+        defaultattributes = ["config", "header", "parameters"]
         for k in self.metadata:
             name = k.strip("/").lower()
             if name in defaultattributes:
                 self.__dict__[name] = self.metadata[k]
 
-        self.boxsize = np.full(3,np.nan)
-
+        self.boxsize = np.full(3, np.nan)
 
     def register_field(self, parttype, name=None, description=""):
         return self.data.register_field(parttype, name=name, description=description)
 
 
-def deepdictkeycopy(olddict,newdict):
+def deepdictkeycopy(olddict, newdict):
     """Recursively walk nested dictionary, only creating empty dictionaries
     for entries that are dictionaries themselves"""
-    for k,v in olddict.items():
-        if isinstance(v,MutableMapping):
+    for k, v in olddict.items():
+        if isinstance(v, MutableMapping):
             newdict[k] = {}
-            deepdictkeycopy(v,newdict[k])
+            deepdictkeycopy(v, newdict[k])
 
 
 class Selector(object):
-    """ Base Class for data selection decorator factory"""
+    """Base Class for data selection decorator factory"""
+
     def __init__(self):
         self.keys = None  # the keys we check for.
         self.data_backup = {}  # holds a copy of the species' fields
@@ -255,11 +284,13 @@ class Selector(object):
             # TODO: Add graceful exit/restore after exception in self.prepare
             self.data_backup = args[0].data
             self.data = {}
-            deepdictkeycopy(self.data_backup,self.data)
+            deepdictkeycopy(self.data_backup, self.data)
 
             self.prepare(*args, **kwargs)
             if self.keys is None:
-                raise NotImplementedError("Subclass implementation needed for self.keys!")
+                raise NotImplementedError(
+                    "Subclass implementation needed for self.keys!"
+                )
             if kwargs.pop("dropkeys", True):
                 for k in self.keys:
                     kwargs.pop(k, None)
@@ -268,6 +299,7 @@ class Selector(object):
                 return result
             finally:
                 self.finalize(*args, **kwargs)
+
         return newfn
 
     def prepare(self, *args, **kwargs):
@@ -275,4 +307,3 @@ class Selector(object):
 
     def finalize(self, *args, **kwargs):
         args[0].data = self.data_backup
-
