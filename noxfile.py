9c36c3591d8215169a5b2c586238eac3165ae7d7
Chris Byrohl <chris.byrohl@mailbox.org>
2022-12-24 23:39:48 +0100

    add coverage in nox

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,26 @@
-from nox_poetry import session
+from nox_poetry import session, Session
+from pathlib import Path
 
 
-@session(python=["3.10", "3.9"])
+python_versions = ["3.10", "3.9", "3.8"]
+
+
+@session(python=python_versions)
 def tests(session):
-    session.install("pytest", ".")
+    session.install("coverage[toml]", "pytest", "pygments")
+    session.install(".")
     session.run("pytest", "-v")
+    try:
+        session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
+    finally:
+        if session.interactive:
+            session.notify("coverage", posargs=[])
+
+
+@session(python=python_versions[0])
+def coverage(session: Session) -> None:
+    args = session.posargs or ["report"]
+    session.install("coverage[toml]")
+    if not session.posargs and any(Path().glob(".coverage.*")):
+        session.run("coverage", "combine")
+    session.run("coverage", *args)
