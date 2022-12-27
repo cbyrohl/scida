1d68f2dd0c92ee5242cea34eaa33b7f8f2a94208
Chris Byrohl <chris.byrohl@mailbox.org>
2022-12-27 13:41:05 +0100

    add misc funcs

---
+++ src/astrodask/misc.py
@@ -0,0 +1,17 @@
+def map_interface_args(paths, *args, **kwargs):
+    """Map arguments for interface if they are not lists"""
+    n = len(paths)
+    for i, path in enumerate(paths):
+        targs = []
+        for arg in args:
+            if not (isinstance(arg, list)) or len(arg) != n:
+                targs.append(arg)
+            else:
+                targs.append(arg[i])
+        tkwargs = {}
+        for k, v in kwargs.items():
+            if not (isinstance(v, list)) or len(v) != n:
+                tkwargs[k] = v
+            else:
+                tkwargs[k] = v[i]
+        yield path, targs, tkwargs
