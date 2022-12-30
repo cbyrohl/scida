a267698d63477e97f2aebf84c5019eaeaef0a94b
Chris Byrohl <chris.byrohl@mailbox.org>
2022-12-30 15:35:45 +0100

    find most specific class to use in load()

--- src/astrodask/convenience.py
+++ src/astrodask/convenience.py
@@ -1,4 +1,7 @@
 import os
+from collections import Counter
+from functools import reduce
+from inspect import getmro
 from os.path import join
 from typing import List
 
@@ -26,17 +29,25 @@ def load(path: str, strict=False, **kwargs):
         raise ValueError("Specified path unknown.")
 
     available_dtypes: List[str] = []
-    for k, dtype in dataset_type_registry.items():
+    reg = dataset_type_registry
+    for k, dtype in reg.items():
         if dtype.validate_path(path):
             available_dtypes.append(k)
-            print("AVAIL:", k)
-        else:
-            print("UNAVAIL:", k)
     if len(available_dtypes) == 0:
         raise ValueError("Unknown data type.")
-    elif strict and len(available_dtypes) > 1:
-        raise ValueError("Ambiguous data type.")
+    if len(available_dtypes) > 1:
+        # reduce candidates by looking at most specific ones.
+        inheritancecounters = [Counter(getmro(reg[k])) for k in available_dtypes]
+        # try to find candidate that shows up only once across all inheritance trees.
+        # => this will be the most specific candidate(s).
+        count = reduce(lambda x, y: x + y, inheritancecounters)
+        available_dtypes = [k for k in available_dtypes if count[reg[k]] == 1]
+        if len(available_dtypes) > 1:
+            # after looking for the most specific candidate(s), do we still have multiple?
+            if strict:
+                raise ValueError("Ambiguous data type.")
+            else:
+                print("Note: Multiple dataset candidates: ", available_dtypes)
     dtype = available_dtypes[0]
-    # TODO: Check for most specific class to take if ambiguous.
 
     return dataset_type_registry[dtype](path, **kwargs)
