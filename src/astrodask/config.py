721aeddf6ae01722d6d61e22a100ea62134b444a
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2023-01-11 19:16:10 +0100

    add cached config

--- src/astrodask/config.py
+++ src/astrodask/config.py
@@ -2,8 +2,11 @@ import os
 
 import yaml
 
+_conf = dict()
 
-def get_config():
+
+def get_config(reload=False):
+    global _conf
     prefix = "ASTRODASK_"
     envconf = {
         k.replace(prefix, "").lower(): v
@@ -13,11 +16,14 @@ def get_config():
     path = envconf.pop("config_path", None)
     if path is None:
         path = os.path.join(os.path.expanduser("~"), ".astrodask.yaml")
+    if not reload and len(_conf) > 0:
+        return _conf
     config = {}
     if os.path.isfile(path):
         with open(path, "r") as file:
             config = yaml.safe_load(file)
     config.update(**envconf)
+    _conf = config
     return config
 
 
