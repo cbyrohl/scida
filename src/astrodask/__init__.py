078862d39b181476d5c939afc665b97fce408258
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2023-01-13 10:44:37 +0100

    make sure areposimulation is being registered

--- src/astrodask/__init__.py
+++ src/astrodask/__init__.py
@@ -4,3 +4,4 @@ from astrodask.convenience import load
 from astrodask.interface import BaseSnapshot
 from astrodask.interfaces.arepo import ArepoSnapshot
 from astrodask.interfaces.illustris import IllustrisSnapshot
+from astrodask.series import ArepoSimulation
