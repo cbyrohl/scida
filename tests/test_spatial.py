0461f29c5773c09d173576b8804ffa019779b257
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2023-03-24 13:36:54 +0100

    fix for catalogs in spatial arepo datasets

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -1,10 +1,9 @@
-from astrodask.convenience import load
-from tests.testdata_properties import require_testdata_path
+from tests.testdata_properties import require_testdata
 
 
-@require_testdata_path("interface", only=["TNG50-4_snapshot", "TNG50-4_group"])
-def test_coordsalias(testdatapath):
-    obj = load(testdatapath)
+@require_testdata("areposnapshot_withcatalog")
+def test_coordsalias(testdata_areposnapshot_withcatalog):
+    obj = testdata_areposnapshot_withcatalog  # load(testdatapath)
     assert obj.file is not None
     assert obj.data is not None
     for ptype in ["PartType%i" % i for i in range(6)] + ["Group", "Subhalo"]:
