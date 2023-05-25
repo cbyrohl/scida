124743055e829ba8b5140bf5665274d93a38b0a6
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2023-05-25 18:43:59 +0200

    extend testdata extraction to series

--- tests/testdata_extractor.py
+++ tests/testdata_extractor.py
@@ -1,7 +1,9 @@
 # some script allowing us to copy HDF5 test files, but truncating fields to length 1
 import os.path
+import pathlib
 
 import h5py
+import numpy as np
 import typer
 
 from astrodask import load
@@ -29,12 +31,50 @@ def create_hdf5_testfile(src: str, dst: str):
         with h5py.File(src, "r") as g:
             for path, shape, dtype in datasets:
                 shape_new = (1,)
-                if len(shape) > 1:
-                    shape_new = (1,) + shape[1:]
-                f.create_dataset(path, shape=shape_new, dtype=dtype, data=g[path][0])
+                tmpdata = None
+                # vlen HDF5 dataset
+                dt = g[path].dtype
+                print(path, dt, h5py.check_vlen_dtype(dt))
+                if h5py.check_vlen_dtype(dt):
+                    # do not know how to treat, so skip for now
+                    dtype = np.int32
+                    shape_new = (1,)
+                    tmpdata = np.array([0], dtype=dtype)
+                else:
+                    if len(shape) > 1:
+                        shape_new = (1,) + shape[1:]
+                        tmpdata = g[path][0]
+                    elif len(shape) == 1:
+                        shape_new = (1,)
+                        tmpdata = g[path][0]
+                    if len(shape) == 0:
+                        # this is a scalar, no slicing
+                        shape_new = ()
+                        tmpdata = g[path]
+                print(path, shape, shape_new, tmpdata.shape)
+                f.create_dataset(path, shape=shape_new, dtype=dtype, data=tmpdata)
         for path, dct in attrs.items():
             for k in dct:
                 f[path].attrs[k] = dct[k]
+
+
+@app.command()
+def minimize_series_hdf5(src: str, dst: str):
+    """We minimize the series data by copying the folder structure,
+    copying&finding all HDF5 files and truncating their record lengths."""
+    pathlib.Path(dst).mkdir(exist_ok=True)
+    for root, dirs, files in os.walk(src):
+        print("=== %s ===" % root)
+        for d in dirs:
+            pathlib.Path(os.path.join(dst, root[len(src) + 1 :], d)).mkdir(
+                exist_ok=True
+            )
+        hdf5files = [f for f in files if f.endswith(".hdf5")]
+        for fn in hdf5files:
+            srcfn = os.path.join(root, fn)
+            dstfn = os.path.join(dst, srcfn[len(src) + 1 :])
+            print(srcfn, "->", dstfn)
+            create_hdf5_testfile(srcfn, dstfn)
 
 
 if __name__ == "__main__":
