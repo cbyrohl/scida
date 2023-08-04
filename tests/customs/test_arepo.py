b0fd5681d6431921990fa6b8a32352aae31dbbb6
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2023-08-04 18:03:52 +0200

    add test for unbound selector

---
+++ tests/customs/test_arepo.py
@@ -0,0 +1,16 @@
+import dask.array as da
+from testdata_properties import require_testdata_path
+
+from scida import load
+
+
+@require_testdata_path("interface", only=["TNG50-4_snapshot"])
+def test_selector(testdatapath):
+    hid = 41
+    obj = load(testdatapath, units=False)
+    d = obj.return_data(haloID=hid)
+    assert da.all(d["PartType0"]["GroupID"] == hid).compute()
+
+    d = obj.return_data(unbound=True)
+    # unbound gas has groupid = max_int64 = 9223372036854775807
+    assert da.all(d["PartType0"]["GroupID"] == 9223372036854775807).compute()
