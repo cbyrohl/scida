98a08209423231f9611bf5fc1e7cd68195711ac6
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2023-12-12 13:49:38 +0100

    series/utilities docstrings

--- src/scida/utilities.py
+++ src/scida/utilities.py
@@ -1,9 +1,26 @@
+"""Some utility functions"""
+
 import zarr
 
 from .interface import Dataset
 
 
 def copy_to_zarr(fp_in, fp_out, compressor=None):
+    """
+    Reads and converts a scida Dataset to a zarr object on disk
+    Parameters
+    ----------
+    fp_in: str
+        object path to convert
+    fp_out: str
+        output path
+    compressor:
+        zarr compressor to use, see https://zarr.readthedocs.io/en/stable/tutorial.html#compressors
+
+    Returns
+    -------
+
+    """
     ds = Dataset(fp_in)
     compressor_dflt = zarr.storage.default_compressor
     zarr.storage.default_compressor = compressor
